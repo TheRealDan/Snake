ff0c864fa7083983288ac6c9297f6213c0caa0d4
TheRealDan <thedanielhailwood@gmail.com>
2021-06-07 10:47:27 +1200

    MainMenu + Game Screens

---
+++ core/src/dev/therealdan/snake/GameScreen.java
@@ -0,0 +1,62 @@
+package dev.therealdan.snake;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class GameScreen implements Screen {
+
+    final SnakeGame game;
+
+    private ScreenViewport viewport;
+    private OrthographicCamera camera;
+
+    public GameScreen(SnakeGame game) {
+        this.game = game;
+
+        camera = new OrthographicCamera();
+        viewport = new ScreenViewport(camera);
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0.2f, 1);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        // todo - do render
+        game.batch.end();
+
+        // todo - do user input
+
+        // todo - do logics
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+}
---
+++ core/src/dev/therealdan/snake/MainMenuScreen.java
@@ -0,0 +1,64 @@
+package dev.therealdan.snake;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class MainMenuScreen implements Screen {
+
+    final SnakeGame game;
+
+    private ScreenViewport viewport;
+    private OrthographicCamera camera;
+
+    public MainMenuScreen(final SnakeGame game) {
+        this.game = game;
+
+        camera = new OrthographicCamera();
+        viewport = new ScreenViewport(camera);
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0.15f, 0.2f, 1);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Touch to start", 0, 0);
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+}
