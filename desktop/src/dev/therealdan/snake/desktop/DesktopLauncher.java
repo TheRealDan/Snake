0e7548d1f29ba19833f9ea708ed7dcb42e8001fd
TheRealDan <thedanielhailwood@gmail.com>
2021-06-07 10:10:10 +1200

    create new empty libgdx project

---
+++ desktop/src/dev/therealdan/snake/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dev.therealdan.snake.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dev.therealdan.snake.Snake;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Snake(), config);
+	}
+}
