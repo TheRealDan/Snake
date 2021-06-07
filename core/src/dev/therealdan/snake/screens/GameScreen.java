6eb076a0a50c025d953717d6863e8ac26234467f
TheRealDan <thedanielhailwood@gmail.com>
2021-06-07 17:36:17 +1200

    add import

--- core/src/dev/therealdan/snake/screens/GameScreen.java
+++ core/src/dev/therealdan/snake/screens/GameScreen.java
@@ -1,5 +1,6 @@
 package dev.therealdan.snake.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
