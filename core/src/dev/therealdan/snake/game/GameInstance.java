e5700c6bffd2b92cc00a05821d95c1769be0b550
TheRealDan <thedanielhailwood@gmail.com>
2021-06-07 19:28:39 +1200

    refactor: move abstracted game logic into GameInstance

---
+++ core/src/dev/therealdan/snake/game/GameInstance.java
@@ -0,0 +1,48 @@
+package dev.therealdan.snake.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class GameInstance {
+
+    private Random random;
+
+    public Snake snake;
+    public List<Apple> apples = new ArrayList<>();
+
+    private long lastAppleSpawn = System.currentTimeMillis();
+    private long appleSpawnInterval = 2500;
+
+    public GameInstance() {
+        random = new Random();
+
+        snake = new Snake(Color.GREEN, 4);
+    }
+
+    public void loop(float delta) {
+        snake.handleMovementControls(delta);
+        snake.handleConnectedBody(delta);
+
+        handleConsumeApples();
+        handleAppleSpawning();
+    }
+
+    private void handleConsumeApples() {
+        for (Apple apple : new ArrayList<>(apples)) {
+            if (snake.overlaps(apple)) {
+                apples.remove(apple);
+                snake.addBody();
+            }
+        }
+    }
+
+    private void handleAppleSpawning() {
+        if (System.currentTimeMillis() - lastAppleSpawn > appleSpawnInterval) return;
+        lastAppleSpawn = System.currentTimeMillis();
+        apples.add(new Apple((random.nextBoolean() ? 1 : -1) * random.nextInt(Gdx.graphics.getWidth() / 2), (random.nextBoolean() ? 1 : -1) * random.nextInt(Gdx.graphics.getHeight() / 2)));
+    }
+}
--- core/src/dev/therealdan/snake/screens/GameScreen.java
+++ core/src/dev/therealdan/snake/screens/GameScreen.java
@@ -1,41 +1,29 @@
 package dev.therealdan.snake.screens;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import dev.therealdan.snake.game.Apple;
-import dev.therealdan.snake.game.Snake;
+import dev.therealdan.snake.game.GameInstance;
 import dev.therealdan.snake.main.SnakeGame;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 public class GameScreen implements Screen {
 
     final SnakeGame game;
 
-    private Random random;
     private ScreenViewport viewport;
     private OrthographicCamera camera;
 
-    private Snake snake;
-    private List<Apple> apples = new ArrayList<>();
-
-    private long lastAppleSpawn = System.currentTimeMillis();
-    private long appleSpawnInterval = 2500;
+    private GameInstance instance;
 
     public GameScreen(SnakeGame game) {
         this.game = game;
 
-        random = new Random();
         camera = new OrthographicCamera();
         viewport = new ScreenViewport(camera);
 
-        snake = new Snake(Color.GREEN, 4);
+        instance = new GameInstance();
     }
 
     @Override
@@ -48,26 +36,12 @@ public class GameScreen implements Screen {
 
         game.shapeRenderer.setAutoShapeType(true);
         game.shapeRenderer.begin();
-        for (Apple apple : apples)
+        for (Apple apple : instance.apples)
             apple.render(game.shapeRenderer);
-        snake.render(game.shapeRenderer);
+        instance.snake.render(game.shapeRenderer);
         game.shapeRenderer.end();
 
-        snake.handleMovementControls(delta);
-        snake.handleConnectedBody(delta);
-
-        for (Apple apple : apples) {
-            if (snake.overlaps(apple)) {
-                apples.remove(apple);
-                snake.addBody();
-                break;
-            }
-        }
-
-        if (System.currentTimeMillis() - lastAppleSpawn > appleSpawnInterval) {
-            lastAppleSpawn = System.currentTimeMillis();
-            apples.add(new Apple((random.nextBoolean() ? 1 : -1) * random.nextInt(Gdx.graphics.getWidth() / 2), (random.nextBoolean() ? 1 : -1) * random.nextInt(Gdx.graphics.getHeight() / 2)));
-        }
+        instance.loop(delta);
     }
 
     @Override
