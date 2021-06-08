5ed19ab00a2462c502a0a6e227892c542886bcf9
TheRealDan <thedanielhailwood@gmail.com>
2021-06-08 17:02:10 +1200

    improve apple art

--- core/src/dev/therealdan/snake/game/Apple.java
+++ core/src/dev/therealdan/snake/game/Apple.java
@@ -6,18 +6,30 @@ import com.badlogic.gdx.math.Circle;
 
 public class Apple extends Circle {
 
-    private Color color;
+    private Color red;
+    private Color green;
 
     public Apple(float x, float y) {
-        color = Color.RED;
-        radius = 8;
         this.x = x;
         this.y = y;
+        radius = 8;
+        red = new Color(1.0f, 0.13333334f, 0.047058824f, 1);
+        green = new Color(0.6901961f, 0.85882354f, 0.2627451f, 1);
     }
 
     public void render(ShapeRenderer shapeRenderer) {
-        shapeRenderer.setColor(color);
         shapeRenderer.set(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(red);
         shapeRenderer.circle(x, y, radius);
+
+        shapeRenderer.set(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(Color.BLACK);
+        shapeRenderer.circle(x, y, radius);
+
+        shapeRenderer.line(x, y + radius - 1, x, y + radius + 3);
+
+        shapeRenderer.set(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(green);
+        shapeRenderer.ellipse(x, y + radius - 1, radius, radius / 2f);
     }
 }
