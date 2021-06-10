5c88ea4e2cadabe6b8867bf63109819774ededdb
TheRealDan <thedanielhailwood@gmail.com>
2021-06-10 19:22:09 +1200

    update build.bat to include tag and build

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
 call gradlew desktop:dist
-del Snake.jar
-del builds\Snake.jar
-rename desktop\build\libs\desktop-latest.jar Snake.jar
-robocopy desktop\build\libs\ builds\ Snake.jar
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`git describe --tags`) DO (SET describe=%%F)
+FOR /f "tokens=1 delims=-" %%a in ("%describe%") do (SET tag=%%a)
+FOR /f "tokens=2 delims=-" %%a in ("%describe%") do (SET build=%%a)
+
+robocopy desktop\build\libs\ builds\ desktop-latest.jar
+rename builds\desktop-latest.jar "Snake %tag%.%build%.jar"
